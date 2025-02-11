0784af175e5820cc0c5e335880a6bb298ccd4fad
tarang-mcode <tarang.mcodeinfosoft@gmail.com>
2025-02-11 19:00:32 +0530

    [DEN-324] splash icons.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import {Colors} from './Src/Theme';
 import NavigationWrapper from './Src/Navigation/index.tsx';
 import {AuthProvider} from './Src/Providers/authProvider.tsx';
+import BootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   const backgroundStyle = {
     backgroundColor: Colors.white,
     flex: 1,
