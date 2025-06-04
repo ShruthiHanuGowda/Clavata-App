0e95f4e68b2327f36f2bfd6b7ee03abcf839b905
Jemin Ramani <jemin@d.energy>
2025-06-04 10:59:22 +0530

    fix pdf download issue

--- Src/Screens/MarketPlace/OffsetScreen/index.tsx
+++ Src/Screens/MarketPlace/OffsetScreen/index.tsx
@@ -215,10 +215,7 @@ const OffsetScreen = ({ route }: any) => {
 
   const handleDownloadCertificate = async () => {
     try {
-      // if (!pdfDownloadUrl) return;
-      let pdfDownloadUrl = "https://nfts-data.s3.me-central-1.amazonaws.com/redemption_statements/01JWQNY3D1TARZVV3JV9298MBQ.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAVRUVV6WQJST3SLWS%2F20250602%2Fme-central-1%2Fs3%2Faws4_request&X-Amz-Date=20250602T064051Z&X-Amz-Expires=86400&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDkaDG1lLWNlbnRyYWwtMSJGMEQCIDdTB%2BeIzoUr86JCvdNxRVp49lGNwvB1BeDB5eLx%2Fx4iAiAo2azSF937crPeaMX1dNazY2Nds0beVBgVu%2FPxFIt4tyqKAwjo%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDM4MTQ5MjI2MjMwNCIMT75Yz4HBR55LzpA0Kt4C7aPuIXQmWMSUGb%2FpRFRNFFck65Lvmoi6VxfJ5ZjyKCxwzRx3umASA%2FutY9qbpi7XobRPppTwhLr4zB3X64Vfg6PZr337ccN2sCSwVs4DKEOZaeioPTOFoSIhg5%2BvQskK1Sztlhh2uD7cBDeuEpm0Ur%2F0tMmv7PiWD0oWhjix91jo5de3WsOfDTcaDuFcfjq8VKZBpmDe8dxEKQd0nkipwKa3CWCQvFI7RrFBBOyWxBkZTjvwE72xhKVns813c31v6ECwkplvpnZPN9Rg9n%2FCVvC9frwPEKMGk08emYY5EsOWHeYtKriaHnjiKlFBsFuwdgu6Rihf8qmbSJPQaRoYsYwDFN04tbVItKFTbDG4qbbqFV4YcoFmj%2BePgHNI%2B8ColBvjkkkukbe%2BBq%2Bp0lEZ5YjilaZ15j%2FQxrTXghMqPF5lBIMU5sCJJ3f%2BOWMdh7hySLcjaY0owa%2FFGgmBXFUw6o71wQY6nwGn5d6Eq5IVyWenzQNp3K6htG4cZ%2FPvKm9zOInF3e%2BUG1a4Jj%2Bfe7qjy%2B4lgE3UsFOEf%2F7mOOk5Iv291A5x6%2F%2FdKAv2z1LN8unbu5qGgRkv28SlStPZK7tcwDoKzyOHBK0%2FLSnFNu9HnY67e9RSHvijrQulcluqJ8dXA3Hwg9y4NYarw2xGm8Rg%2BBL99tyhUuoFzon7BZJMvYApGrgGoC8%3D&X-Amz-Signature=e09e6d9d2aa1f151dd5a075f82d235f57e443dfe47c562898b86cb63da87edbb&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22redemption_statement_01JWQNY3D1TARZVV3JV9298MBQ.pdf%22&response-content-type=application%2Fpdf"
-      console.log('Downloading certificate from:', pdfDownloadUrl);
-
+      if (!pdfDownloadUrl) return;
 
       const timestamp = Math.floor(Date.now() / 1000);
       const fileName = `certificate_${timestamp}.pdf`;
